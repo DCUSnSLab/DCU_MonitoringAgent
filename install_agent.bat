69afc6442504086eb142f1bf19c98a46bda217a8
soobinjeon <marsberry@cu.ac.kr>
2026-04-22 21:38:04 +0900

    설치파일 생성 및 수정 완료

--- install_agent.bat
+++ install_agent.bat
@@ -21,7 +21,35 @@ echo oLink.Save >> %SCRIPT%
 cscript /nologo %SCRIPT%
 del %SCRIPT%
 
-echo 4. Starting Agent...
+echo 4. Applying Chrome Remote Debugging Settings...
+set PS_SCRIPT="%TEMP%\PatchChrome.ps1"
+echo $portArgs = " --remote-debugging-port=9222 --user-data-dir=C:\chrome-debug" > %PS_SCRIPT%
+echo $shell = New-Object -COM WScript.Shell >> %PS_SCRIPT%
+echo $desktopPath = [Environment]::GetFolderPath('Desktop') >> %PS_SCRIPT%
+echo $publicDesktop = [Environment]::GetFolderPath('CommonDesktopDirectory') >> %PS_SCRIPT%
+echo $taskbarPath = "$env:APPDATA\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar" >> %PS_SCRIPT%
+echo $startMenuPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs" >> %PS_SCRIPT%
+echo $publicStartMenu = "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs" >> %PS_SCRIPT%
+echo $shortcuts = Get-ChildItem -Path $desktopPath, $publicDesktop, $taskbarPath, $startMenuPath, $publicStartMenu -Filter "*Chrome*.lnk" -Recurse -ErrorAction SilentlyContinue >> %PS_SCRIPT%
+echo foreach ($lnk in $shortcuts) { >> %PS_SCRIPT%
+echo     $s = $shell.CreateShortcut($lnk.FullName) >> %PS_SCRIPT%
+echo     if ($s.Arguments -notmatch "remote-debugging-port") { >> %PS_SCRIPT%
+echo         $s.Arguments = $s.Arguments + $portArgs >> %PS_SCRIPT%
+echo         $s.Save() >> %PS_SCRIPT%
+echo     } >> %PS_SCRIPT%
+echo } >> %PS_SCRIPT%
+echo $regPath = 'HKCU:\Software\Classes\ChromeHTML\shell\open\command' >> %PS_SCRIPT%
+echo if (Test-Path $regPath) { >> %PS_SCRIPT%
+echo     $val = (Get-ItemProperty -Path $regPath).'(default)' >> %PS_SCRIPT%
+echo     if ($val -notmatch "remote-debugging-port") { >> %PS_SCRIPT%
+echo         $newVal = $val.Replace('"%%1"', '--remote-debugging-port=9222 --user-data-dir=C:\chrome-debug "%%1"') >> %PS_SCRIPT%
+echo         Set-ItemProperty -Path $regPath -Name '(default)' -Value $newVal >> %PS_SCRIPT%
+echo     } >> %PS_SCRIPT%
+echo } >> %PS_SCRIPT%
+powershell -NoProfile -ExecutionPolicy Bypass -File %PS_SCRIPT%
+del %PS_SCRIPT%
+
+echo 5. Starting Agent...
 start "" "%TARGET_DIR%\DCU_MonitoringAgent.exe"
 
 echo Installation Complete! Check your system tray.
